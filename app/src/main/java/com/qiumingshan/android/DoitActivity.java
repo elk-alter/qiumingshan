e65a3dd609e5c98d9c13e6f639530d753658615c
Elk <peiyaoren@gmail.com>
2019-06-17 12:23:55 +0800

    添加了布局文件和主要的页面逻辑//TODO

---
+++ app/src/main/java/com/qiumingshan/android/DoitActivity.java
@@ -0,0 +1,15 @@
+package com.qiumingshan.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doit);
+
+
+    }
+}
---
+++ app/src/main/java/com/qiumingshan/android/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.qiumingshan.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView link_signup = findViewById(R.id.link_signup);
+        link_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button_login = findViewById(R.id.button_login);
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
